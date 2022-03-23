4cfe1c20976de3f720920140a6ac5129bc99b1db
Kauã Maximino <kaua_maximino@outlook.com>
2022-03-23 12:36:22 -0300

    requisito 2 ok

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,9 @@ const About = () => (
         This application simulates a Pokédex, a
         digital encyclopedia containing all Pokémons
       </p>
-      <p>One can filter Pokémons by type, and see more details for each one of them</p>
+      <p >One can filter Pokémons by type, and see more details for each one of them</p>
       <img
-        className="pokedex-image"
+        className = 'pokedex-image'
         src={ `https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png` }
         alt="Pokédex"
       />
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+// it('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testa o componente About', () => {
+  it('Testa se a página contém 2 parágrafos as informações sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    const infoPokedex1 = screen.getByText(
+      /This application simulates a Pokédex, a digital encyclopedia containing all/i,
+    );
+
+    const infoPokedex2 = screen.getByText(
+      /One can filter Pokémons by type, and see more details for each one of them/i,
+    );
+
+    expect(infoPokedex1).toBeDefined();
+    expect(infoPokedex2).toBeDefined();
+  });
+
+  it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const heading = screen.getByRole('heading', { level: 2, name: /About Pokédex/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Testa se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const urlImage = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imgAbout = screen.getByRole('img');
+    expect(imgAbout.src).toBe(urlImage);
+  });
+});
